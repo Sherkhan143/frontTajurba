5b6fe67d1354e9414d5dc9abf1bc1ec20d404e49
sherkhan143 <kmoin116@gmail.com>
2023-03-17 17:42:04 +0530

    connection data updated

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,17 @@
 const mysql = require('mysql');
 const con = mysql.createConnection({
-    host : "localhost",
-    user : "root",
-    password : "root",
-    database : "tajurba",
-    port : 3306
+    // host : "localhost",
+    // user : "root",
+    // password : "root",
+    // database : "tajurba",
+    // port : 3306
+
+    host : "sql10.freesqldatabase.com",
+    user: "sql10606488",
+    password: "AmHEigxuQp",
+    database:"sql10606488",
+    port : 3306,
+    multipleStatements:true
 });
 
 con.connect((err) => {
